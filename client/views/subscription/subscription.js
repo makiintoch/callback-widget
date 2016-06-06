be8326d3a216a5a189c2a32fb3fed4817b99cbcf
makiintoch <makiintoch@gmail.com>
2016-06-06 16:32:28 +0300

    Edit subscribe

--- client/views/subscription/subscription.js
+++ client/views/subscription/subscription.js
@@ -1,24 +1,36 @@
 Template.subscription.helpers({
   widgetsList: function() {
-    return Widgets.find();
+  	var widget = Widgets.find(),
+  		option = '<option disabled selected>Выберите виджет</option>';
+
+	widget.forEach(function(item) {
+
+	  option += '<option value="'+item._id+'">'+item.name+'</option>';
+	}); 
+
+    return option;
   },
   getSignature: function() {
   	return Session.get('signatureField')
   },
+  getPaymentAmount: function() {
+  	return 300;
+  },
   getFields: function() {
-  	var key = '7a7261585c525f685638615f536b74685f5b5776703770716e474e';
+  	var key = '7a7261585c525f685638615f536b74685f5b5776703770716e474e',
+  	    widgetId = 123,
+  	    smsCount = 123,
+  	    paymentAmount = 10.00;
 
 	var fields = {
 	  WMI_MERCHANT_ID: '131114291158',
-	  WMI_PAYMENT_AMOUNT: '10.00',
+	  WMI_PAYMENT_AMOUNT: paymentAmount,
 	  WMI_CURRENCY_ID: '643',
-	  WMI_DESCRIPTION: 'BASE64:' + CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse('Оплата демонстрационного заказа')),
+	  WMI_DESCRIPTION: 'BASE64:' + CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse('Оплата смс-рассылки')),
 	  WMI_SUCCESS_URL: 'http://call-message.com/w1/paid',
 	  WMI_FAIL_URL: 'http://call-message.com/w1/fail',
-	  // Если требуется задать только определенные способы оплаты, раскоментируйте данную строку и перечислите требуемые способы оплаты.
-	  // WMI_PTENABLED: ['UnistreamRUB', 'SberbankRUB', 'RussianPostRUB'],
-	  widgetId: 'widgetId',
-	  smsCount: 'smsCount'
+	  widgetId: widgetId,
+	  smsCount: smsCount
 	};
 
 	var comparator = function(a, b){
